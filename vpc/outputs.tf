5d8fedf3a041d85f0736f4aba8234f8ded3b33f5
vamsi <vamsibattu3015@gmail.com>
2025-06-08 11:14:21 +0530

    committed

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -25,3 +25,7 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
     value = values(aws_subnet.database)[*].id
 }
+
+output "aws_db_subnet_group_name" {
+    value = aws_db_subnet_group.main.name
+}
